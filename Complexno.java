b4703575f6126706fb7727692be4d6018c33a653
Vedant Dhote <85186983+Vedant-code@users.noreply.github.com>
2022-03-09 12:14:35 +0530

    Create Complexno.java

---
+++ Complexno.java
@@ -0,0 +1,39 @@
+class complex2 {
+  int real;
+  int image;
+  public
+          complex2(int p, int q){
+              real=p;
+              image=q;
+          }
+          void printcomplex(){
+              System.out.println(real+"+"+image+"i");
+              
+          }
+          static complex2 addcomplex(complex2 A, complex2 B){
+             
+              complex2 temp=new complex2(A.real+B.real, A.image+B.image);
+              
+              return temp;
+          }
+          static complex2 subcomplex(complex2 A, complex2 B){
+             
+              complex2 t=new complex2(A.real-B.real, A.image-B.image);
+              
+              return t;
+          }
+}
+
+class driver{
+    public static void main(String args[]){
+        complex2 A = new complex2(2, 5);
+        complex2 B= new complex2(4, 8);
+        A.printcomplex();
+        B.printcomplex();
+        complex2 C=complex2.addcomplex(A, B);
+        C.printcomplex();
+        complex2 D=complex2.subcomplex(A, B);
+        D.printcomplex();
+        
+    }
+}
